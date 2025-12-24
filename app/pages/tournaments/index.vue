5e7cef6642b4e8a127d3040e04d557a4b7ffe786
andreism <markoandreis07@gmail.com>
2025-12-24 13:54:46 +0100

    Refactor tournaments page

---
+++ app/pages/tournaments/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const { data: tournaments, error, status } = await useFetch('/api/tournaments', {
+    lazy: true,
+});
+</script>
+
+<template>
+    <h1>Tournaments</h1>
+    <article 
+        v-for="tournament in tournaments" 
+        :key="tournament.id"
+    >
+        <NuxtLink :to="`/tournaments/${tournament.id}`">
+            {{ tournament.name }}
+        </NuxtLink>
+    </article>
+</template>
