eece8e8a52e75a484f2483119a4a78a4c63a8e38
andreism <markoandreis07@gmail.com>
2025-12-24 13:53:25 +0100

    Add setup to try out tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	modules: ["@nuxt/eslint"],
 	devtools: { enabled: true },
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 	compatibilityDate: "2025-07-15",
-  css: ["@picocss/pico"],
+	css: ["@picocss/pico"],
+	// css: ["./app/assets/css/main.css"],
 	eslint: {
 		config: {
 			stylistic: {
